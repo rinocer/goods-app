89ad297f6c73021c219a3c8dcc0d1ce4465dbdf3
Marian C Moldovan <moldovan0991@gmail.com>
2016-02-26 22:17:07 +0100

    Added some things... Smart location library in module

---
+++ GivrsApp/smartlocation/src/test/java/io/nlopez/smartlocation/ActivityRecognitionControlTest.java
@@ -0,0 +1,106 @@
+package io.nlopez.smartlocation;
+
+import android.content.Context;
+import android.location.Location;
+
+import com.google.android.gms.location.DetectedActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RuntimeEnvironment;
+
+import io.nlopez.smartlocation.activity.config.ActivityParams;
+import io.nlopez.smartlocation.rx.ObservableFactory;
+import io.nlopez.smartlocation.util.MockActivityRecognitionProvider;
+import io.nlopez.smartlocation.util.MockLocationProvider;
+import io.nlopez.smartlocation.utils.Logger;
+import rx.Observable;
+import rx.observers.TestSubscriber;
+
+import org.robolectric.annotation.Config;
+
+import java.util.Collections;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+@RunWith(CustomTestRunner.class)
+@Config(manifest = Config.NONE)
+public class ActivityRecognitionControlTest {
+
+    private static final ActivityParams DEFAULT_PARAMS = ActivityParams.NORMAL;
+
+    private MockActivityRecognitionProvider mockProvider;
+    private OnActivityUpdatedListener activityUpdatedListener;
+
+    @Before
+    public void setup() {
+        mockProvider = mock(MockActivityRecognitionProvider.class);
+        activityUpdatedListener = mock(OnActivityUpdatedListener.class);
+    }
+
+    @Test
+    public void test_activity_recognition_control_init() {
+        Context context = RuntimeEnvironment.application.getApplicationContext();
+        SmartLocation smartLocation = new SmartLocation.Builder(context).preInitialize(false).build();
+        SmartLocation.ActivityRecognitionControl activityRecognitionControl = smartLocation.activity(mockProvider);
+
+        verifyZeroInteractions(mockProvider);
+
+        smartLocation = new SmartLocation.Builder(context).build();
+        activityRecognitionControl = smartLocation.activity(mockProvider);
+        verify(mockProvider).init(eq(context), any(Logger.class));
+    }
+
+    @Test
+    public void test_activity_recognition_control_start_defaults() {
+        SmartLocation.ActivityRecognitionControl activityRecognitionControl = createActivityRecognitionControl();
+
+        activityRecognitionControl.start(activityUpdatedListener);
+        verify(mockProvider).start(activityUpdatedListener, DEFAULT_PARAMS);
+    }
+
+    @Test
+    public void test_activity_recognition_get_last_location() {
+        SmartLocation.ActivityRecognitionControl activityRecognitionControl = createActivityRecognitionControl();
+        activityRecognitionControl.getLastActivity();
+
+        verify(mockProvider).getLastActivity();
+    }
+
+    @Test
+    public void test_activity_recognition_stop() {
+        SmartLocation.ActivityRecognitionControl activityRecognitionControl = createActivityRecognitionControl();
+        activityRecognitionControl.stop();
+
+        verify(mockProvider).stop();
+    }
+
+    @Test
+    public void test_observable_activity() {
+        TestSubscriber<DetectedActivity> testSubscriber = new TestSubscriber<>();
+        MockActivityRecognitionProvider provider = new MockActivityRecognitionProvider();
+        Observable<DetectedActivity> activityObservable = ObservableFactory.from(
+                SmartLocation.with(RuntimeEnvironment.application.getApplicationContext())
+                        .activity(provider)
+        );
+        activityObservable.subscribe(testSubscriber);
+
+        DetectedActivity detectedActivity = new DetectedActivity(DetectedActivity.UNKNOWN,100);
+        provider.fakeEmitActivity(detectedActivity);
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertReceivedOnNext(Collections.singletonList(detectedActivity));
+    }
+
+    private SmartLocation.ActivityRecognitionControl createActivityRecognitionControl() {
+        Context context = RuntimeEnvironment.application.getApplicationContext();
+        SmartLocation smartLocation = new SmartLocation.Builder(context).preInitialize(false).build();
+        SmartLocation.ActivityRecognitionControl activityRecognitionControl = smartLocation.activity(mockProvider);
+        return activityRecognitionControl;
+    }
+
+}
---
+++ GivrsApp/smartlocation/src/test/java/io/nlopez/smartlocation/location/config/LocationParamsTest.java
@@ -0,0 +1,30 @@
+package io.nlopez.smartlocation.location.config;
+
+import io.nlopez.smartlocation.CustomTestRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by mrm on 10/1/15.
+ */
+@RunWith(CustomTestRunner.class)
+@Config(manifest = Config.NONE)
+public class LocationParamsTest {
+
+    private static final LocationAccuracy ACCURACY = LocationAccuracy.HIGH;
+    private static final long INTERVAL = 1000;
+    private static final float DISTANCE = 1000f;
+    private static final double DELTA = 1e-7;
+
+    @Test
+    public void test_location_params_builder() {
+        LocationParams locationParams = new LocationParams.Builder()
+                .setAccuracy(ACCURACY).setInterval(INTERVAL).setDistance(DISTANCE).build();
+
+        Assert.assertEquals(locationParams.getAccuracy(), ACCURACY);
+        Assert.assertEquals(locationParams.getDistance(), DISTANCE, DELTA);
+        Assert.assertEquals(locationParams.getInterval(), INTERVAL);
+    }
+}
