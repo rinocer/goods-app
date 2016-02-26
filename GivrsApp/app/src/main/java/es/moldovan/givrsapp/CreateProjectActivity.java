d82c58e6a78b4970e3fe71391e2cc44c0e67db41
Marian C Moldovan <moldovan0991@gmail.com>
2016-02-27 00:53:37 +0100

    Form created

--- GivrsApp/app/src/main/java/es/moldovan/givrsapp/CreateProjectActivity.java
+++ GivrsApp/app/src/main/java/es/moldovan/givrsapp/CreateProjectActivity.java
@@ -1,11 +1,16 @@
 package es.moldovan.givrsapp;
 
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
@@ -16,16 +21,28 @@ import com.mapbox.mapboxsdk.views.MapView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import es.moldovan.givrsapp.objs.ListQuery;
 import io.nlopez.smartlocation.OnLocationUpdatedListener;
 import io.nlopez.smartlocation.SmartLocation;
+import me.gujun.android.taggroup.TagGroup;
 
 public class CreateProjectActivity extends AppCompatActivity {
 
     @Bind(R.id.mapview)
     MapView mapView;
 
+    @Bind(R.id.tag_group)
+    TagGroup tagGroup;
+
+    @Bind(R.id.scrollView)
+    ScrollView scrollView;
+
+    @Bind(R.id.createProjectBanner)
+    TextView bannerTextView;
+
     private Marker marker;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +50,15 @@ public class CreateProjectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_project);
         ButterKnife.bind(this);
 
+        setTitle("Create a new cause");
+
         setUpMap();
         mapView.onCreate(savedInstanceState);
+
+        tagGroup.setTags(new String[]{"Hugs"});
+        tagGroup.clearFocus();
+        bannerTextView.requestFocus();
+
 
     }
 
@@ -43,7 +67,21 @@ public class CreateProjectActivity extends AppCompatActivity {
         mapView.setStyleUrl(Style.EMERALD);
         mapView.setCenterCoordinate(new LatLng(38.91318, -77.03257));
         mapView.setZoomLevel(10);
-
+        mapView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        scrollView.requestDisallowInterceptTouchEvent(true);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        scrollView.requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+                return mapView.onTouchEvent(event);
+            }
+        });
         SmartLocation.with(this).location()
                 .oneFix()
                 .start(new OnLocationUpdatedListener() {
@@ -96,11 +134,22 @@ public class CreateProjectActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+        progressDialog.dismiss();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    @OnClick(R.id.createProjectSubmit)
+    public void click(){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Just a sec");
+        progressDialog.setMessage("Work in progress");
+        progressDialog.setProgressStyle(progressDialog.STYLE_SPINNER);
+        progressDialog.show();
+
     }
 }
