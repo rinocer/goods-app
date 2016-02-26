689f15209e0a55d268dbe9f5fcc0657272d05fa9
Marian C Moldovan <moldovan0991@gmail.com>
2016-02-26 17:05:26 +0100

    Java objects...

---
+++ GivrsApp/app/src/main/java/es/moldovan/givrsapp/objs/Operation.java
@@ -0,0 +1,12 @@
+package es.moldovan.givrsapp.objs;
+
+/**
+ * Created by marian.claudiu on 26/2/16.
+ */
+public class Operation {
+    private String operation;
+
+    public Operation(String operation){
+        this.operation = operation;
+    }
+}
