d6cfdc0315daf31be040b07e9d390e64e25504a5
Ovidiu <ovidiu5891@gmail.com>
2016-02-26 19:03:28 +0100

    Projects view

--- GivrsApp/app/src/main/java/es/moldovan/givrsapp/MainActivity.java
+++ GivrsApp/app/src/main/java/es/moldovan/givrsapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -35,11 +37,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import es.moldovan.givrsapp.objs.ListQuery;
 import es.moldovan.givrsapp.objs.Project;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.my_recycler_view)
+    RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private static final String TAG = "MainActivity";
 
@@ -52,8 +60,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- GivrsApp/app/src/main/java/es/moldovan/givrsapp/ProjectAdapter.java
+++ GivrsApp/app/src/main/java/es/moldovan/givrsapp/ProjectAdapter.java
@@ -6,25 +6,39 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import es.moldovan.givrsapp.objs.Project;
 
 /**
  * Created by OvidiuMircea on 26/02/2016.
  */
 public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ViewHolder> {
-    private String[] mDataset;
+    private List<Project> dataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
+
+
+
+
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
-
+        @Bind(R.id.imageViewProject)
+        ImageView imageViewProject;
+        @Bind(R.id.textViewTitle)
+        TextView textViewTitle;
+        @Bind(R.id.textViewContent)
+        TextView textViewContent;
+        @Bind(R.id.textViewName)
+        TextView textViewName;
+        @Bind(R.id.textViewDistance)
+        TextView textViewDistance;
         public ViewHolder(View v) {
             super(v);
             ButterKnife.bind(this, v);
@@ -32,8 +46,8 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ViewHold
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public ProjectAdapter(String[] myDataset) {
-        mDataset = myDataset;
+    public ProjectAdapter(List<Project> dataset) {
+        this.dataset = dataset;
     }
 
     // Create new views (invoked by the layout manager)
@@ -42,33 +56,37 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ViewHold
                                                         int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_proj, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        //and so on
+                .inflate(R.layout.item_project, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        final ProjectItem feed = dataset.get(position);
-        holder.textViewTitle.setText(feed.getTitle());
-        holder.textViewContent.setText(Html.fromHtml(feed.getDescription().replaceAll("</?img[^>]*?>", "")));
-        if (feed.getImageUrl() != null && feed.getImageUrl().length() > 0) {
-            holder.imageViewFeed.setVisibility(View.VISIBLE);
-            Picasso.with(holder.imageViewFeed.getContext()).load(feed.getImageUrl()).into(holder.imageViewFeed);
+        final Project project = dataset.get(position);
+        holder.textViewTitle.setText(project.getName());
+        if (project.getImage() != null && project.getImage().length() > 0) {
+            holder.imageViewProject.setVisibility(View.VISIBLE);
+            Picasso.with(holder.imageViewProject.getContext()).load(project.getImage()).into(holder.imageViewProject);
             holder.textViewTitle.setTextColor(Color.WHITE);
             holder.textViewTitle.setShadowLayer(1f, 2f, 2f, Color.DKGRAY);
         } else {
-            holder.imageViewFeed.setVisibility(View.GONE);
+            holder.imageViewProject.setVisibility(View.GONE);
             holder.textViewTitle.setShadowLayer(0f, 0f, 0f, Color.BLACK);
             holder.textViewTitle.setTextColor(Color.BLACK);
         }
+        holder.textViewName.setText(project.getInitiator());
+        holder.textViewContent.setText(project.getDescription());
+        holder.textViewDistance.setText(this.getDistance());
+
+    }
+
+    private CharSequence getDistance() {
+        return null;
     }
 
     @Override
     public int getItemCount() {
         return dataset.size();
     }
-
 }
